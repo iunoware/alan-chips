bc56115a6c7c3cb8fcb5337805e43bd16f21dac1
UmarAbdullah79 <uu0339843@gmail.com>
2026-02-24 15:12:53 +0530

    ht access added

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+    ],
+  },
+
+  experimental: {
+    optimizePackageImports: ["gsap"],
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;

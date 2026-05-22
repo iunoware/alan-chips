061b925110d48036a8366031501034a3f50137db
Praveen123-png <praveenkumar814444@gmail.com>
2026-05-22 18:54:45 +0530

    layout tsx modified, navbar modified, still no buttons are working

--- src/components/pageComponents/home/Hero2.tsx
+++ src/components/pageComponents/home/Hero2.tsx
@@ -30,49 +30,79 @@ export default function Hero2() {
   );
 
   return (
-    <div className="relative">
-      <section
-        ref={containerRef}
-        className="flex relative overflow-x-clip flex-col justify-center items-center h-screen w-full bg-center bg-cover"
+    // <div className="relative">
+    // <section
+    //   ref={containerRef}
+    //   className="flex relative overflow-x-clip flex-col justify-center items-center h-screen w-full bg-center bg-cover"
+    // >
+    //   <video
+    //     autoPlay
+    //     muted
+    //     playsInline
+    //     className="w-full h-full absolute inset-0 object-cover object-center"
+    //   >
+    //     <source src="/videos/alan-chips-video-2.mp4" type="video/mp4" />
+    //     Your browser does not support the video tag.
+    //   </video>
+
+    //   <div className="absolute inset-0 flex flex-col justify-center items-center pointer-events-none">
+    //     <h1 className="fade-in-text uppercase font-semibold text-center mb-3 opacity-90 bg-linear-to-r from-orange to-amber-400 bg-clip-text text-transparent tracking-widest text-6xl">
+    //       Alan Chips
+    //     </h1>
+
+    //     <h2 className="fade-in-text text-xl opacity-90 font-serif mb-1 text-orange">
+    //       Crafted Taste, Preserved Since 1960
+    //     </h2>
+
+    //     <div className="left-0 w-50 mb-4 h-0.5 bg-linear-to-r from-orange via-gold to-orange" />
+
+    //     <p className="fade-in-text max-w-lg opacity-90 font-sans text-center text-gray-800">
+    //       From a trusted legacy in South Tamil Nadu to a premium experience for everyone.
+    //     </p>
+
+    //     <div className="fade-in-text mt-4">
+    //       <Link href="/about" className="uppercase text-orange cursor-pointer">
+    //         Explore our story
+    //       </Link>
+
+    //       <div className="left-0 w-44 mb-4 h-0.5 bg-linear-to-r from-transparent via-orange to-transparent" />
+    //     </div>
+    //   </div>
+    // </section>
+
+    <section
+      ref={containerRef}
+      className="flex relative overflow-x-clip flex-col justify-center items-center h-screen w-full bg-center bg-cover"
+    >
+      <video
+        autoPlay
+        muted
+        playsInline
+        className="w-full h-full absolute inset-0 object-cover object-center"
       >
-        <div>
-          <video
-            autoPlay
-            muted
-            playsInline
-            className="w-full h-full absolute inset-0 object-cover object-center"
-          >
-            <source src="/videos/alan-chips-video-2.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        <source src="/videos/alan-chips-video-2.mp4" type="video/mp4" />
+      </video>
 
-          <div className="absolute inset-0 flex flex-col justify-center items-center">
-            <h1 className="fade-in-text uppercase font-semibold text-center mb-3 opacity-90 bg-linear-to-r from-orange to-amber-400 bg-clip-text text-transparent tracking-widest text-6xl">
-              Alan Chips
-            </h1>
-
-            {/* <span className="text-sm">since 1960</span> */}
-            <h2 className="fade-in-text text-xl opacity-90 font-serif mb-1 text-orange">
-              Crafted Taste, Preserved Since 1960
-            </h2>
-
-            <div className="left-0 w-50 mb-4 h-0.5 bg-linear-to-r from-orange via-gold to-orange" />
-
-            <p className="fade-in-text max-w-lg opacity-90 font-sans text-center text-gray-800">
-              From a trusted legacy in South Tamil Nadu to a premium experience for
-              everyone.
-            </p>
-
-            <div className="fade-in-text mt-4">
-              <Link href="/about" className="uppercase text-orange">
-                Explore our story
-              </Link>
-
-              <div className="left-0 w-44 mb-4 h-0.5 bg-linear-to-r from-transparent via-orange to-transparent" />
-            </div>
-          </div>
+      <div className="absolute inset-0 flex flex-col justify-center items-center pointer-events-none">
+        <h1 className="fade-in-text uppercase font-semibold text-center mb-3 opacity-90 bg-linear-to-r from-orange to-amber-400 bg-clip-text text-transparent tracking-widest text-6xl">
+          Alan Chips
+        </h1>
+        <h2 className="fade-in-text text-xl opacity-90 font-serif mb-1 text-orange">
+          Crafted Taste, Preserved Since 1960
+        </h2>
+        <div className="left-0 w-50 mb-4 h-0.5 bg-linear-to-r from-orange via-gold to-orange" />
+        <p className="fade-in-text max-w-lg opacity-90 font-sans text-center text-gray-800">
+          From a trusted legacy in South Tamil Nadu to a premium experience for everyone.
+        </p>
+        <div className="fade-in-text mt-4 pointer-events-auto">
+          <Link href="/about" className="uppercase text-orange">
+            Explore our story
+          </Link>
+          <div className="left-0 w-44 mb-4 h-0.5 bg-linear-to-r from-transparent via-orange to-transparent" />
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+
+    // </div>
   );
 }
